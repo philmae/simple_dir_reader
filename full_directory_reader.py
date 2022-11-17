ad553ce9c82d1f90a155e1995fe36d738d295580
Philip Mahr <philip.maehr@gmail.com>
2022-11-17 09:34:25 -0300

    v1

---
+++ full_directory_reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#coding=utf-8
+
+'''
+
+Reads the directory and outputs all sub-dir and
+files within into a .txt file in a clean style
+
+'''
+
+import os
+
+absolute_path = os.path.dirname(__file__)
+
+with open("output.txt", "w", newline='', encoding="utf-8") as a:
+    for path, subdirs, files in os.walk(absolute_path):
+        a.write('.\\' + os.path.relpath(path + os.linesep, start=absolute_path)) 
+        for filename in files:
+            a.write('\t%s\n' % filename)
+print("Done")
